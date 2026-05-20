6866a44cd5f61463da02a3ce44f63c499b98e52e
Sans6063 <santiagolopez1119066@correo.itm.edu.co>
2026-05-20 17:07:38 -0500

    Phase 6 Completed - Standings

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddScoped<ITournamentService, TournamentService>();
 builder.Services.AddScoped<IMatchService, MatchService>();
 builder.Services.AddScoped<IMatchEventService, MatchEventService>();
 builder.Services.AddScoped<MatchValidationHelper>();
+builder.Services.AddScoped<IStandingsService, StandingsService>();
 
 
 // ── AutoMapper ──
